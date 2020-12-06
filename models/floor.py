6f4dca97f5e05b239246835015fd776953eb4ad6
Gonxolo <gonxa.au@gmail.com>
2020-12-06 09:05:19 -0300

    new floor class

---
+++ models/floor.py
@@ -0,0 +1,40 @@
+import numpy as np
+from OpenGL.GL import *
+
+import lib.basic_shapes as bs
+import lib.easy_shaders as es
+import lib.transformations as tr
+
+
+# FLOOR
+class Floor():
+
+    def __init__(self):
+        self.GPUfloor = es.toGPUShape(bs.createTextureNormalsCube('img/background_tile.png'), GL_REPEAT, GL_NEAREST)
+        self.transform = tr.matmul([tr.translate(0,0,-10),tr.scale(20,20,1),tr.uniformScale(1)])
+    
+    def draw(self, pipeline, projection, view):             
+
+        # White light in all components: ambient, diffuse and specular.
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "La"), 0.55, 0.55, 0.55)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ld"), 0.65, 0.65, 0.65)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ls"), 0.4, 0.4, 0.4)
+
+        # Object is barely visible at only ambient. Bright white for diffuse and specular components.
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ka"), 0.25, 0.25, 0.25)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Kd"), 0.6, 0.6, 0.6)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "Ks"), 0.6, 0.6, 0.6)
+
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "lightPosition"), 0,0 ,20)
+        glUniform3f(glGetUniformLocation(pipeline.shaderProgram, "viewPosition"), 0, 0, 0)
+        glUniform1ui(glGetUniformLocation(pipeline.shaderProgram, "shininess"), 100)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "constantAttenuation"), 0.01)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "linearAttenuation"), 0.01)
+        glUniform1f(glGetUniformLocation(pipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+        glUseProgram(pipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE, self.transform)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+
+        pipeline.drawShape(self.GPUfloor)
